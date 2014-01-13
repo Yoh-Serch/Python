f8662ce71d7cdce48effdb299121d0635083db79
Yoh Serch Azakura <yoh@serch.gateway.2wire.net>
2014-01-13 12:09:10 -0600

    Se modifica por la lectura de un numero y generación de una lista con los valores a combinar

--- conjuntos.py
+++ conjuntos.py
@@ -1,12 +1,4 @@
-cantidad_maxima = int(raw_input("Ingrese la cantidad maxima de digitos"))
-print cantidad_maxima
-numero_combinaciones = 0
-
-if numero_combinaciones == 0:
-	print "{},"
-	numero_combinaciones = numero_combinaciones + 1
-else:
-	for i in range(6):
-		print "Hola"
-
+numero_de_digitos = int(input("El rango de digitos sera de 1- "))
+valores = range(1,numero_de_digitos)
+print valores
 
