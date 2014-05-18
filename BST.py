d5062451d24bfb26f32626fc64cce04ee04a1559
Sergio Durán <def.serch@gmail.com>
2014-05-17 19:37:30 -0500

    Creada clase Nodo

---
+++ BST.py
@@ -0,0 +1,11 @@
+class Nodo:
+    parent = None
+    valor = None
+    izq = None
+    der = None
+    
+    def __init__(self, valor):
+        self.valor = valor
+        
+    def __str__(self):
+        return str(self.valor)
